8be4416920ec9a9df8a90669bdbda729b496b993
Jouni Miettunen <jouni.miettunen@gmail.com>
2019-05-16 09:03:59 +0300

    python black formatted code

--- carthage_dep.py
+++ carthage_dep.py
@@ -10,8 +10,9 @@ clean_names = {
     "usabilla-u4a-ios-swift-sdk": "Usabilla",
     "test-cloud-xcuitest-extensions": "Xamarin\nXCUITest\nextensions",
     "SwinjectStoryboard": "Swinject\nStoryboard",
-    "ios-snapshot-test-case": "iOSSnapshot\nTestCase"
+    "ios-snapshot-test-case": "iOSSnapshot\nTestCase",
 }
+
 
 def find_files(path, filenames):
     found = []
@@ -27,6 +28,7 @@ def find_files(path, filenames):
                 if filename == file.lower():
                     found.append(os.path.join(root, file))
     return found
+
 
 def parse_cartfile(filename, show_version):
     found = []
@@ -46,22 +48,25 @@ def parse_cartfile(filename, show_version):
         if show_version:
             if len(items) > 3 and not items[3].startswith("#"):
                 items[2] = "%s %s" % (items[2], items[3])
-            result.append(items[2].strip('\"'))
+            result.append(items[2].strip('"'))
         found.append(result)
     file.close()
     return found
 
+
 def parse_dependency_name(items):
-    dependency_fullname = items.strip('\"')
+    dependency_fullname = items.strip('"')
     dependency_fullname = clean_dependency_name(dependency_fullname, clean_names)
     dependency = filter(None, dependency_fullname.strip().split("/"))[-1]
     dependency = clean_dependency_name(dependency, clean_names)
     if dependency.endswith((".git", ".json", ".swift")):
-        dependency = dependency[:dependency.rfind(".")]
+        dependency = dependency[: dependency.rfind(".")]
     return dependency
+
 
 def clean_dependency_name(name, dictionary):
     return dictionary[name] if name in dictionary.keys() else name
+
 
 def generate_dot_graph(files, data, show_title):
     lines = []
@@ -72,23 +77,30 @@ def generate_dot_graph(files, data, show_title):
                 lines.append([framework, dependency])
             elif len(item) == 3:
                 (framework, dependency, version) = item
-                lines.append([framework, dependency, "%s\\n%s" % (dependency.split("/")[-1], version)])
+                lines.append(
+                    [
+                        framework,
+                        dependency,
+                        "%s\\n%s" % (dependency.split("/")[-1], version),
+                    ]
+                )
     # lines.sort(key = lambda x:x[1])
 
     # Dot header
     graph_data = "digraph G {\nconcentrate = true\n"
     if show_title:
-        graph_data += "labelloc = t\nlabel = \"" + ",\n".join(files) + "\"\n"
+        graph_data += 'labelloc = t\nlabel = "' + ",\n".join(files) + '"\n'
     graph_data += "\n"
     # Dot graph
     for line in lines:
         if len(line) == 2:
-            graph_data += "\"%s\" -> \"%s\"\n" % (line[0], line[1])
+            graph_data += '"%s" -> "%s"\n' % (line[0], line[1])
         elif len(line) == 3:
-            graph_data += "\"%s\" -> \"%s\" -> \"%s\"\n" % (line[0], line[1], line[2])
+            graph_data += '"%s" -> "%s" -> "%s"\n' % (line[0], line[1], line[2])
     # Dot footer
     graph_data += "}\n"
     return graph_data
+
 
 #####
 
@@ -105,14 +117,14 @@ if len(sys.argv) > 1:
             use_resolved = True
         elif arg.endswith("version"):
             show_version = True
-        elif arg.endswith("files"): # --list-files
+        elif arg.endswith("files"):  # --list-files
             show_title = True
 
 files = []
 if use_resolved:
     files = find_files(path, ["Cartfile.resolved"])
 else:
-   files = find_files(path, ["Cartfile", "Cartfile.private"])
+    files = find_files(path, ["Cartfile", "Cartfile.private"])
 
 found = []
 for filename in files:
